3a204c3a2199c6a2fdff6b5eb02fad168578c338
cendress <95492688+cendress@users.noreply.github.com>
2023-10-06 15:21:47 -0400

    Make custom button & text field

---
+++ GHFollowers/FavoritesListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteLIstViewController.swift
+//  GHFollowers
+//
+//  Created by Christopher Endress on 10/5/23.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
---
+++ GHFollowers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  GHFollowers
+//
+//  Created by Christopher Endress on 10/5/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
