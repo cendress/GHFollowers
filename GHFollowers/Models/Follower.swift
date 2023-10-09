a92723f72c3d7a0066fee95e264d07f17705d912
cendress <95492688+cendress@users.noreply.github.com>
2023-10-09 16:26:35 -0400

    Fix bugs

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Christopher Endress on 10/8/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+  var login: String
+  var avatarUrl: String
+}
