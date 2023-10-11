6caff6b000abe8e9572c8a5b12aab018b9fd6954
cendress <95492688+cendress@users.noreply.github.com>
2023-10-11 09:41:31 -0400

    Clean up FollowersListVC

--- GHFollowers/Controllers/FollowerListViewController.swift
+++ GHFollowers/Controllers/FollowerListViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 class FollowerListViewController: UIViewController {
   
   var username: String!
+  var collectionView: UICollectionView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureViewController()
+    configureCollectionView()
+    getFollower()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    navigationController?.setNavigationBarHidden(false, animated: true)
+  }
+  
+  func configureViewController() {
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
-    
+  }
+  
+  func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+    view.addSubview(collectionView)
+    collectionView.backgroundColor = .systemPink
+    collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.resuseID)
+  }
+  
+  func getFollower() {
     NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-      
       switch result {
       case .success(let followers):
         print(followers)
@@ -27,9 +46,4 @@ class FollowerListViewController: UIViewController {
       }
     }
   }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    navigationController?.setNavigationBarHidden(false, animated: true)
-  }
-  
 }
