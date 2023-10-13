6b5b821329b361bda70d0d84cfc21e568e932353
cendress <95492688+cendress@users.noreply.github.com>
2023-10-13 15:16:03 -0400

    Make a weak reference to self in network manager

--- GHFollowers/Controllers/FollowerListViewController.swift
+++ GHFollowers/Controllers/FollowerListViewController.swift
@@ -58,7 +58,9 @@ class FollowerListViewController: UIViewController {
   }
   
   func getFollower() {
-    NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+    NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+      guard let self = self else { return }
+      
       switch result {
       case .success(let followers):
         self.followers = followers
