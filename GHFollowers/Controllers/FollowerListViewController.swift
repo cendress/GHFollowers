48f5cbc7ea8b9aee44c95b40a33f0157e63ba5f8
cendress <95492688+cendress@users.noreply.github.com>
2023-10-16 15:45:47 -0400

    Present user info screen modally

--- GHFollowers/Controllers/FollowerListViewController.swift
+++ GHFollowers/Controllers/FollowerListViewController.swift
@@ -16,6 +16,7 @@ class FollowerListViewController: UIViewController {
   var filteredFollowers: [Follower] = []
   var page = 1
   var hasMoreFollowers = true
+  var isSearching = false
   
   var collectionView: UICollectionView!
   var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -116,6 +117,14 @@ extension FollowerListViewController: UICollectionViewDelegate {
       getFollowers(username: username, page: page)
     }
   }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let activeArray = isSearching ? filteredFollowers : followers
+    let follower = activeArray[indexPath.item]
+    
+    let destinationVC = UserInfoViewController()
+    present(destinationVC, animated: true)
+  }
 }
 
 extension FollowerListViewController: UISearchResultsUpdating, UISearchBarDelegate {
@@ -123,11 +132,14 @@ extension FollowerListViewController: UISearchResultsUpdating, UISearchBarDelega
   func updateSearchResults(for searchController: UISearchController) {
     guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
     
+    isSearching = true
+    
     filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
     updateData(on: filteredFollowers)
   }
   
   func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
     updateData(on: followers)
+    isSearching = false
   }
 }
---
+++ GHFollowers/Controllers/UserInfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfoViewController.swift
+//  GHFollowers
+//
+//  Created by Christopher Endress on 10/16/23.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+  }
+  
+}
