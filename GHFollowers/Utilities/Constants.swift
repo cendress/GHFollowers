06da4463d9b2924d3a6921efa9d1d80f2bf43363
cendress <95492688+cendress@users.noreply.github.com>
2023-10-18 10:40:33 -0400

    Create constants enum

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Christopher Endress on 10/18/23.
+//
+
+import Foundation
+
+enum SFSymbols {
+  static let location = "mappin.and.ellipse"
+}
