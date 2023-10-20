1421addb36a32d29c86be21db4c97031b97f58d4
cendress <95492688+cendress@users.noreply.github.com>
2023-10-20 14:51:35 -0400

    Complete date label

--- GHFollowers/Controllers/UserInfoViewController.swift
+++ GHFollowers/Controllers/UserInfoViewController.swift
@@ -40,7 +40,7 @@ class UserInfoViewController: UIViewController {
           self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
           self.add(childVC: GFRepoItemViewController(user: user), to: self.itemViewOne)
           self.add(childVC: GFFollowerItemViewController(user: user), to: self.itemViewTwo)
-          self.dateLabel.text = user.createdAt
+          self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
         }
       case .failure(let error):
         self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
