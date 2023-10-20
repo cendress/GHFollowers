e336281009255df13663d8707b15d621b9201762
cendress <95492688+cendress@users.noreply.github.com>
2023-10-20 15:31:30 -0400

    Create UserInfoVCDelegate

--- GHFollowers/Controllers/UserInfoViewController.swift
+++ GHFollowers/Controllers/UserInfoViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+protocol UserInfoVCDelegate: class {
+  func didTapGitHubProfile()
+  func didTapGetFollowers()
+}
+
 class UserInfoViewController: UIViewController {
   
   let headerView = UIView()
@@ -37,15 +42,24 @@ class UserInfoViewController: UIViewController {
       switch result {
       case .success(let user):
         DispatchQueue.main.async {
-          self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
-          self.add(childVC: GFRepoItemViewController(user: user), to: self.itemViewOne)
-          self.add(childVC: GFFollowerItemViewController(user: user), to: self.itemViewTwo)
-          self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
+        self.configureUIElements(with: user)
         }
       case .failure(let error):
         self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
       }
     }
+  }
+  
+  func configureUIElements(with user: User) {
+    let repoItemVC = GFRepoItemViewController(user: user)
+    repoItemVC.delegate = self
+    
+    let followerItemsVC = GFFollowerItemViewController(user: user)
+    
+    self.add(childVC: GFRepoItemViewController(user: user), to: self.itemViewOne)
+    self.add(childVC: GFFollowerItemViewController(user: user), to: self.itemViewTwo)
+    self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
+    self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
   }
   
   func layoutUI() {
@@ -88,4 +102,17 @@ class UserInfoViewController: UIViewController {
   @objc func dismissVC() {
     dismiss(animated: true)
   }
+}
+
+extension UserInfoViewController: UserInfoVCDelegate {
+  
+  func didTapGitHubProfile() {
+    //show safari view controller
+  }
+  
+  func didTapGetFollowers() {
+    //show users followers
+  }
+  
+  
 }
