e76e69afb55312984956d27c763035f24a3fcb1c
cendress <95492688+cendress@users.noreply.github.com>
2023-10-22 17:41:16 -0400

    Create custom favorite cell

---
+++ GHFollowers/CustomViews/Cells/FavoriteCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteCell.swift
+//  GHFollowers
+//
+//  Created by Christopher Endress on 10/22/23.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
