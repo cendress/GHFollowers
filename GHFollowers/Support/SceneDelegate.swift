4210cb1e9f80546bfb662c50b053657e85002cc3
cendress <95492688+cendress@users.noreply.github.com>
2023-10-23 16:49:35 -0400

    Declutter code

--- GHFollowers/Support/SceneDelegate.swift
+++ GHFollowers/Support/SceneDelegate.swift
@@ -21,34 +21,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     window = UIWindow(frame: windowScene.coordinateSpace.bounds)
     window?.windowScene = windowScene
-    window?.rootViewController = createTabbar()
+    window?.rootViewController = GFTabBarController()
     window?.makeKeyAndVisible()
     
     configureNavigationBar()
-  }
-  
-  func createSearchNavigationController() -> UINavigationController {
-    let searchViewController = SearchViewController()
-    searchViewController.title = "Search"
-    searchViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-    
-    return UINavigationController(rootViewController: searchViewController)
-  }
-  
-  func createFavoritesNavigationController() -> UINavigationController {
-    let favoritesListViewController = FavoritesListViewController()
-    favoritesListViewController.title = "Favorites"
-    favoritesListViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-    
-    return UINavigationController(rootViewController: favoritesListViewController)
-  }
-  
-  func createTabbar() -> UITabBarController {
-    let tabbar = UITabBarController()
-    UITabBar.appearance().tintColor = .systemGreen
-    tabbar.viewControllers = [createSearchNavigationController(), createFavoritesNavigationController()]
-    
-    return tabbar
   }
   
   func configureNavigationBar() {
