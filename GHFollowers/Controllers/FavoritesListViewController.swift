91cf2effa4062aaf9a784cfbed9ac41986c6471d
cendress <95492688+cendress@users.noreply.github.com>
2023-10-23 16:17:15 -0400

    Create table view datasource methods

--- GHFollowers/Controllers/FavoritesListViewController.swift
+++ GHFollowers/Controllers/FavoritesListViewController.swift
@@ -23,14 +23,34 @@ class FavoritesListViewController: UIViewController {
     navigationController?.navigationBar.prefersLargeTitles = true
   }
   
+  func configureTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
+    
+  }
+  
   func getFavorites() {
     PersistenceManager.retrieveFavorites { result in
       switch result {
       case .success(let favorites):
-        print(favorites)
+        self.favorites = favorites
       case .failure(let error):
         break
       }
     }
+  }
+}
+
+extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    favorites.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID) as! FavoriteCell
+    let favorite = favorites[indexPath.row]
+    cell.set(favorite: favorite)
+    return cell
   }
 }
