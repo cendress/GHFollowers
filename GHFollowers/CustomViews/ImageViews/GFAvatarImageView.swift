7c84211ce9b6620d512b6fb52e22af1e57cf9e9e
cendress <95492688+cendress@users.noreply.github.com>
2023-10-24 13:16:11 -0400

    Update constants

--- GHFollowers/CustomViews/ImageViews/GFAvatarImageView.swift
+++ GHFollowers/CustomViews/ImageViews/GFAvatarImageView.swift
@@ -9,19 +9,17 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
   
-  let cache               = NetworkManager.shared.cache
-  let placeholderImage    = UIImage(named: "avatar-placeholder")!
+  let cache = NetworkManager.shared.cache
+  let placeholderImage = Images.placeHolder
   
   override init(frame: CGRect) {
     super.init(frame: frame)
     configure()
   }
   
-  
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
   
   private func configure() {
     layer.cornerRadius  = 10
