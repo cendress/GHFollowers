6aab06ef8910185a54b10958cd30239085639dd4
cendress <95492688+cendress@users.noreply.github.com>
2023-10-24 12:47:09 -0400

    Refactor code

--- GHFollowers/Controllers/UserInfoViewController.swift
+++ GHFollowers/Controllers/UserInfoViewController.swift
@@ -61,7 +61,7 @@ class UserInfoViewController: UIViewController {
     self.add(childVC: repoItemVC, to: self.itemViewOne)
     self.add(childVC: followerItemsVC, to: self.itemViewTwo)
     self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
-    self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
+    self.dateLabel.text = "GitHub since \(user.createdAt.convertToMonthYearFormat())"
   }
   
   func layoutUI() {
--- GHFollowers/CustomViews/Cells/FollowerCell.swift
+++ GHFollowers/CustomViews/Cells/FollowerCell.swift
@@ -13,23 +13,25 @@ class FollowerCell: UICollectionViewCell {
   let avatarImageView = GFAvatarImageView(frame: .zero)
   let usernameLabel   = GFTitleLabel(textAlignment: .center, fontSize: 16)
   
-  
   override init(frame: CGRect) {
     super.init(frame: frame)
     configure()
   }
   
-  
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  
   func set(follower: Follower) {
     usernameLabel.text = follower.login
-    avatarImageView.downloadImage(from: follower.avatarUrl)
+    NetworkManager.shared.downloadImage(from: follower.avatarUrl) { [weak self] image in
+      guard let self = self else { return }
+      
+      DispatchQueue.main.async {
+        self.avatarImageView.image = image
+      }
+    }
   }
-  
   
   private func configure() {
     addSubview(avatarImageView)
