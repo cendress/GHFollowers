38272da6cbc4e434a55204555c77530ca71cd26c
cendress <95492688+cendress@users.noreply.github.com>
2023-11-01 10:19:34 -0400

    Add SwiftUI

---
+++ GHFollowers/CustomViews/Cells/FollowerCell/FollowerView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowerView.swift
+//  GHFollowers
+//
+//  Created by Christopher Endress on 11/1/23.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  FollowerView()
+}
